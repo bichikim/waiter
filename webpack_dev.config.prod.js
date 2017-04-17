3ddd18c53eeced308c63b102fe7cbd5e4929a715
bichi <bichi@live.co.kr>
2017-04-18 00:34:40 +0900

    Fix main javascript

---
+++ webpack_dev.config.prod.js
@@ -0,0 +1,26 @@
+module.exports = {
+    node: {
+        __dirname: true
+    },
+    entry: './test/app.js',
+    output:{
+        path: __dirname + '/test/',
+        filename: 'index.js'
+    },
+    module:{
+        loaders:[
+            {
+                test:/\.sass$/,
+                loader: 'style!css!sass'
+            }, {
+                test: /\.js/,
+                loader: 'babel-loader',
+                exclude: /(node_modules\bower_components)/,
+                query:{
+                    presets : ['es2015']
+                }
+            }
+        ]
+    },
+    watch: true
+};
